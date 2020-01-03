8f25bb542b74f8d26435032b76a87d5119e6929e
donmahallem <donmahallem@users.noreply.github.com>
2020-01-03 20:39:30 +0100

    bump

--- src/vehicle-db.spec.ts
+++ src/vehicle-db.spec.ts
@@ -2,12 +2,25 @@
  * Source https://github.com/donmahallem/TrapezeApiClientNode
  */
 
-import { IVehicleLocationList } from "@donmahallem/trapeze-api-types";
+import { IVehicleLocationExtended } from "@donmahallem/trapeze-api-client-types";
+import {
+    IVehicleLocationList,
+    TripId,
+    VehicleId,
+} from "@donmahallem/trapeze-api-types";
 import { expect } from "chai";
 import "mocha";
 import * as sinon from "sinon";
 import { VehicleDb } from "./vehicle-db";
-
+/**
+ * Helper method
+ * @param ins
+ * @param vehicles
+ */
+const setVehicles: (ins: VehicleDb, vehicles: Array<Partial<IVehicleLocationExtended>>) => void =
+    (ins: VehicleDb, vehicles: any[]): void => {
+        (ins as any).mVehicles = vehicles;
+    };
 describe("vehicle-db.ts", () => {
     describe("VehicleDb", () => {
         let instance: VehicleDb;
@@ -31,6 +44,64 @@ describe("vehicle-db.ts", () => {
         after(() => {
             sandbox.restore();
             clock.restore();
+        });
+        const testVehiclesId: Array<Partial<IVehicleLocationExtended>> = [
+            { id: "any id1" as VehicleId },
+            { id: 2939 as VehicleId },
+        ];
+        const testVehiclesTripId: Array<Partial<IVehicleLocationExtended>> = [
+            { tripId: "any id2" as TripId },
+            { tripId: 2969 as TripId },
+        ];
+        describe("getVehicleById(id)", () => {
+            it("should return undefined if no item is in the list", () => {
+                setVehicles(instance, []);
+                expect(instance.getVehicleById("id1" as VehicleId)).to.equal(undefined);
+            });
+            it("should return undefined if the queried id is unknown", () => {
+                setVehicles(instance, testVehiclesId);
+                expect(instance.getVehicleById("id1" as VehicleId)).to.equal(undefined);
+            });
+            testVehiclesId.forEach((val: Partial<IVehicleLocationExtended>) => {
+                it("should return element with id '" + val.id + "' if the queried id is unknown", () => {
+                    setVehicles(instance, testVehiclesId);
+                    expect(instance.getVehicleById(val.id as VehicleId)).to.deep.equal(val);
+                });
+            });
+        });
+        describe("getVehicleByTripId(id)", () => {
+            it("should return undefined if no item is in the list", () => {
+                setVehicles(instance, []);
+                expect(instance.getVehicleByTripId("id1" as TripId)).to.equal(undefined);
+            });
+            it("should return undefined if the queried id is unknown", () => {
+                setVehicles(instance, testVehiclesTripId);
+                expect(instance.getVehicleByTripId("id1" as TripId)).to.equal(undefined);
+            });
+            testVehiclesTripId.forEach((val: Partial<IVehicleLocationExtended>) => {
+                it("should return element with tripId '" + val.tripId + "' if the queried id is unknown", () => {
+                    setVehicles(instance, testVehiclesTripId);
+                    expect(instance.getVehicleByTripId(val.tripId as TripId)).to.deep.equal(val);
+                });
+            });
+        });
+        describe("addResponse(resp)", () => {
+            let addAllStub: sinon.SinonStub;
+            let convertResponseStub: sinon.SinonStub;
+            const testValue1: string = "test value 1";
+            const testValue2: string = "test value 2";
+            beforeEach(() => {
+                addAllStub = sandbox.stub(instance, "addAll");
+                convertResponseStub = sandbox.stub(instance, "convertResponse");
+            });
+            it("should call addAll with the result from convertResponse", () => {
+                convertResponseStub.returns(testValue2);
+                instance.addResponse(testValue1 as any);
+                expect(addAllStub.callCount).to.equal(1, "addAll should only be called once");
+                expect(convertResponseStub.callCount).to.equal(1, "convertResponse should only be called once");
+                expect(addAllStub.calledWith(testValue2)).to.equal(true);
+                expect(convertResponseStub.calledWith(testValue1)).to.equal(true);
+            });
         });
         describe("convertResponse(result)", () => {
             const testData: IVehicleLocationList = {
--- src/vehicle-db.ts
+++ src/vehicle-db.ts
@@ -1,10 +1,14 @@
-import {
-    IVehicleLocationList,
-    IVehicleLocation,
-    VehicleId,
-    TripId
-} from "@donmahallem/trapeze-api-types";
+/*!
+ * Source https://github.com/donmahallem/TrapezeApiClientNode
+ */
+
 import { IVehicleLocationExtended } from "@donmahallem/trapeze-api-client-types";
+import {
+    IVehicleLocation,
+    IVehicleLocationList,
+    TripId,
+    VehicleId,
+} from "@donmahallem/trapeze-api-types";
 
 type VehicleIdMap = Map<VehicleId, IVehicleLocationExtended>;
 export class VehicleDb {
@@ -17,8 +21,8 @@ export class VehicleDb {
         return this.mLastUpdate;
     }
     /**
-     * 
-     * @param vehicleResponse 
+     *
+     * @param vehicleResponse
      * @since 3.0.0
      */
     public convertResponse(vehicleResponse: IVehicleLocationList): IVehicleLocationExtended[] {
@@ -45,82 +49,84 @@ export class VehicleDb {
         return [];
     }
     /**
-     * 
-     * @param resp 
+     *
+     * @param resp
      * @since 3.0.0
      */
     public addResponse(resp: IVehicleLocationList): void {
         this.addAll(this.convertResponse(resp));
     }
     /**
-     * 
-     * @param locations 
+     *
+     * @param locations
      * @since 3.0.0
      */
     public addAll(locations: IVehicleLocationExtended[]): void {
-        const dataMap: VehicleIdMap = locations.reduce<VehicleIdMap>((prev: VehicleIdMap, cur: IVehicleLocationExtended): VehicleIdMap => {
-            if (prev.has(cur.id)) {
-                const curEntry: IVehicleLocationExtended | undefined = prev.get(cur.id);
-                if (curEntry && curEntry.lastUpdate >= cur.lastUpdate) {
-                    return prev;
+        const dataMap: VehicleIdMap = locations
+            .reduce<VehicleIdMap>((prev: VehicleIdMap, cur: IVehicleLocationExtended): VehicleIdMap => {
+                if (prev.has(cur.id)) {
+                    const curEntry: IVehicleLocationExtended | undefined = prev.get(cur.id);
+                    if (curEntry && curEntry.lastUpdate >= cur.lastUpdate) {
+                        return prev;
+                    }
                 }
-            }
-            if (this.ttl < 0 || cur.lastUpdate + this.ttl > Date.now())
-                prev.set(cur.id, cur);
-            return prev;
-        }, new Map<VehicleId, IVehicleLocationExtended>());
+                if (this.ttl < 0 || cur.lastUpdate + this.ttl > Date.now()) {
+                    prev.set(cur.id, cur);
+                }
+                return prev;
+            }, new Map<VehicleId, IVehicleLocationExtended>());
         this.mVehicles = Array.from(dataMap.values());
         this.mLastUpdate = this.mVehicles
-            .reduce((prev: number, cur: IVehicleLocationExtended): number => {
-                return Math.max(prev, cur.lastUpdate);
-            }, 0);
+            .reduce((prev: number, cur: IVehicleLocationExtended): number =>
+                Math.max(prev, cur.lastUpdate), 0);
 
     }
 
     /**
-     * 
-     * @param id 
+     *
+     * @param id
      * @since 3.0.0
      */
     public getVehicleById(id: VehicleId): IVehicleLocationExtended | undefined {
-        const idx: number = this.mVehicles.findIndex((value: IVehicleLocationExtended): boolean => {
-            return value.id === id;
-        });
+        const idx: number = this.mVehicles.findIndex((value: IVehicleLocationExtended): boolean =>
+            value.id === id);
         return idx < 0 ? undefined : this.mVehicles[idx];
     }
     /**
-     * 
-     * @param id 
+     *
+     * @param id
      * @since 3.0.0
      */
     public getVehicleByTripId(id: TripId): IVehicleLocationExtended | undefined {
-        const idx: number = this.mVehicles.findIndex((value: IVehicleLocationExtended): boolean => {
-            return value.tripId === id;
-        });
+        const idx: number = this.mVehicles.findIndex((value: IVehicleLocationExtended): boolean =>
+            value.tripId === id);
         return idx < 0 ? undefined : this.mVehicles[idx];
     }
 
     /**
-     * 
+     *
      * @param since
      * @since 3.0.0
      */
     public getVehicles(since: number = 0): IVehicleLocationExtended[] {
         return this.mVehicles
-            .filter((vehicle: IVehicleLocationExtended): boolean => {
-                return vehicle.lastUpdate >= since;
-            });
+            .filter((vehicle: IVehicleLocationExtended): boolean =>
+                vehicle.lastUpdate >= since);
     }
     /**
-     * 
-     * @param left 
-     * @param right 
-     * @param top 
-     * @param bottom 
+     *
+     * @param left
+     * @param right
+     * @param top
+     * @param bottom
      * @param since
-     * @since 3.0.0 
+     * @since 3.0.0
      */
-    public getVehiclesIn(left: number, right: number, top: number, bottom: number, since: number = 0): IVehicleLocationExtended[] {
+    public getVehiclesIn(left: number,
+                         right: number,
+                         top: number,
+                         bottom: number,
+                         since: number = 0): IVehicleLocationExtended[] {
         if (left >= right) {
             throw new Error("left must be smaller than right");
         }
@@ -129,10 +135,12 @@ export class VehicleDb {
         }
         return this.mVehicles
             .filter((vehicle: IVehicleLocationExtended): boolean => {
-                if (vehicle.longitude < left || vehicle.longitude > right)
+                if (vehicle.longitude < left || vehicle.longitude > right) {
                     return false;
-                if (vehicle.latitude < bottom || vehicle.latitude > top)
+                }
+                if (vehicle.latitude < bottom || vehicle.latitude > top) {
                     return false;
+                }
                 return vehicle.lastUpdate >= since;
             });
     }
